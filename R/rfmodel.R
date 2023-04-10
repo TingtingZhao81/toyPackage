a5e3b065d7ad5d338165954bfc86882135ab6ca7
TingtingZhao81 <sunshinetingtingzhao@gmail.com>
2023-04-10 11:32:04 -0700

    add rf model into the data

---
+++ R/rfmodel.R
@@ -0,0 +1,9 @@
+#' Pretrained ML model
+#'
+#' A model trained from PubChem
+#'
+#' @format ## `rfmodel`
+#' A random forest model
+#' mz0, int2_o_int0, int1_o_int0, mz2_0, mz1_0
+#' @source
+"rfmodel"
